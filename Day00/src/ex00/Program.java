e1b46fe7348e99fce80f7ef48b04b8aebbbfa3ab
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-02-12 22:30:20 +0300

    fix: type paramentrs

--- Day00/src/ex00/Program.java
+++ Day00/src/ex00/Program.java
@@ -2,8 +2,8 @@ package ex00;
 
 public class Program {
     public static void main(String[] args) {
-        Integer number = 479598;
-        Integer sum = 0;
+        int number = 479598;
+        int sum = 0;
 
         for (int i = number; i != 0; i /= 10) {
             sum += i % 10;
